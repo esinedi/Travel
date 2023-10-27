169c8e880ef7ec4aac947db374f210c99066ab97
esinedi <1347374188@qq.com>
2023-10-27 16:02:58 +0800

    添加less全局样式

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      // 给 less-loader 传递 Less.js 相关选项
+      less: {
+        additionalData: `@import "@/assets/styles/varibles.less";`,
+      }
+    }
+  }
 })
