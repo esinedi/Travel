3261e0dc714040a151c63e39fd0a57a99816d394
esinedi <1347374188@qq.com>
2023-10-27 16:01:53 +0800

    添加inconfont和首页头部

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HomeHeaders></HomeHeaders>
+    <!-- <router-link to="/list">列表页</router-link> -->
+  </div>
+</template>
+
+<script>
+
+import HomeHeaders from './components/Header.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    HomeHeaders
+  }
+}
+</script>
---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      内容
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe62d;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  height: .86rem;
+  color: #fff;
+  background: @over-bg-color;
+  .header-left {
+    width: .64rem;
+    float: left;
+    font-size: .5rem;
+    text-align: center;
+  }
+  .header-input {
+    flex: 1;
+    margin-left: .2rem;
+    padding-left: .1rem;
+    height: .64rem;
+    color: #ccc;
+    line-height: .64rem;
+    background: #fff;
+    border-radius: .1rem;
+  }
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+    .iconfont {
+      margin-left: -.04rem;
+      font-size: .24rem;
+    }
+  }
+}
+</style>
