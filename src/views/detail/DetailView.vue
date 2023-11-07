7166432b7e0933ee68ab2a34d2f18f472fbba9b4
esinedi <1347374188@qq.com>
2023-11-07 15:40:45 +0800

    add detail top

--- src/views/detail/DetailView.vue
+++ src/views/detail/DetailView.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="detail">
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
+import DetailHeader from './components/Header.vue'
 
 export default {
   name: 'DetailView',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.content {
+  height: 50rem;
+}
 </style>
